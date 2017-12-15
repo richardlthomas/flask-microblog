e0d60a747a64a8977355d4e6a8b8da48ff610189
Rick Thomas <thomas.richard.l@gmail.com>
2017-12-15 03:12:38 -0500

    Initial commit through Chapter 3.6

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Rick'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Durham!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Rick'},
+            'body': 'Check out this content'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
